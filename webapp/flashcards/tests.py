8ad89c0603abe88d727db5df877cf173cfad084f
Junaid Ali <mailtojunaid@gmail.com>
2019-01-19 21:19:25 -0600

    added integration test for index view

--- webapp/flashcards/tests.py
+++ webapp/flashcards/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Category
 
 # Create your tests here.
+class CategoryModelTests(TestCase):
+    def test_no_categories(self):
+        """
+        If no categories are defined, than homepage shows default message
+        """
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Welcome')
+    
+    def test_one_category(self):
+        """
+        If one category exists, it shoudl be displayed on homepage
+        """
+        category1 = Category(name_en="DemoCategory", name_ar="عرض")
+        category1.save()
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'DemoCategory')
+        self.assertContains(response, 'عرض')
