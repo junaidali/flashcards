bcb583e2cc37922d7132b2e3464cb10c65937386
Junaid Ali <mailtojunaid@gmail.com>
2019-02-01 20:14:16 -0600

    added category view to list all words.

--- webapp/flashcards/views.py
+++ webapp/flashcards/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Category, Word
 
@@ -7,3 +7,10 @@ def index(request):
     categories = Category.objects.all()
     context = {'categories': categories}
     return render(request, 'flashcards/index.html.j2', context)
+
+# Return all questions for category
+def detail(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    words = Word.objects.filter(categories=category)
+    context = {'category': category, 'words': words}
+    return render(request, 'flashcards/words.html.j2', context)
